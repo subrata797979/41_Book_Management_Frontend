ff769c82c5d21a71d96450c5d0315af7aa5ce7c2
subrata797979 <subrata797979@gmail.com>
2021-12-31 02:16:48 +0530

    books fetched in frontend

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BooksService } from './../../services/books.service';
+import { AppService } from "../../app.service";
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-books',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService, private bookService: BooksService) {}
+  ASSET_URL = environment.apiUrl;
+  bookList: any;
 
   ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(){
+    this.bookService.getBooks().subscribe((data)=>{
+      console.log(data.data);
+      this.bookList=data.data;
+    })
   }
 
 }
