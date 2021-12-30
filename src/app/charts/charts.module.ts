d6f804b8d966f3c27d0029685aba42ec186dcae6
subrata797979 <subrata797979@gmail.com>
2021-12-30 11:58:20 +0530

    sidebar fix

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
-import { HighchartsChartModule } from "highcharts-angular";
 
 const components = [PieChartComponent, LineChartComponent];
 
@@ -10,8 +9,7 @@ const components = [PieChartComponent, LineChartComponent];
 @NgModule({
   declarations: components,
   imports: [
-    CommonModule,
-    HighchartsChartModule
+    CommonModule
   ],
   exports: components,
 })
