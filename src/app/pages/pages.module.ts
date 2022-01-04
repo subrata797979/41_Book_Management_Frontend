44bfd4563db0cc90a9bccef724d1b6283f9f1efe
subrata797979 <subrata797979@gmail.com>
2022-01-05 00:17:02 +0530

    form modification

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { AddBookComponent } from './add-book/add-book.component';
 import { AppMaterialModule } from '../app-material.module';
 import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '../components/components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const pages = [DashboardComponent, MainComponent, BooksComponent, AddBookComponent];
 
@@ -16,7 +17,9 @@ const pages = [DashboardComponent, MainComponent, BooksComponent, AddBookCompone
     CommonModule,
     AppMaterialModule,
     RouterModule,
-    ComponentsModule
+    ComponentsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: pages,
 })
