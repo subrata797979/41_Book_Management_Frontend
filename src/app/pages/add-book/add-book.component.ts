99e5b9f94f54839ddf61c76f5ce878d500604031
subrata797979 <subrata797979@gmail.com>
2022-01-04 23:54:32 +0530

    form validatoin on going

--- src/app/pages/add-book/add-book.component.ts
+++ src/app/pages/add-book/add-book.component.ts
@@ -1,5 +1,6 @@
+import { BooksService } from './../../services/books.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-book',
@@ -7,21 +8,37 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent implements OnInit {
-  bookForm: any;
 
-  constructor() { }
+  bookForm: any;
+  constructor(private service: BooksService) { }
 
   ngOnInit(): void {
-    this.createForm();
+    this.setForm();
     this.setChangeValidate();
   }
 
-  createForm() {
-    
+  setForm() {
+    this.bookForm = new FormGroup({
+      _id: new FormControl("", null),
+      title: new FormControl("", null),
+      isbn: new FormControl("", null),
+      pageCount: new FormControl("", null),
+      publishedDate: new FormControl("", null),
+      thumbnailUrl: new FormControl("", null),
+      shortDescription: new FormControl("", null),
+      longDescription: new FormControl("", null),
+      status: new FormControl("", null),
+      authors: new FormControl("", null),
+      categories: new FormControl("", null),
+    });
   }
 
   setChangeValidate() {
     
+  }
+
+  createBook() {
+
   }
 
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -12,12 +12,38 @@ export class BooksService {
   constructor(private http: HttpClient, private router: Router) {}
   API_URL = environment.apiUrl;
 
+  createBook(data: any) {
+    this.http
+      .post(this.API_URL + "/books", data)
+      .subscribe((res) => {
+        if (res) {
+          this.router.navigate(["/dashboard-books"]);
+        }
+      });
+  }
+  
   getBooks() {
     const model: Subject<any> = new Subject<any>();
     this.http.get(this.API_URL + "/books").subscribe((res) => {
       model.next({ data: res });
     });
     return model;
+  }
+
+  editBook(id: any, data: any) {
+    this.http
+      .put(this.API_URL + "/book/" + id, data)
+      .subscribe((res) => { });
+  }
+
+  deleteBook(id: any) {
+    this.http
+      .delete(this.API_URL + "/book/" + id, id)
+      .subscribe((res) => {
+        if (res) {
+          this.router.navigate(["/dashboard-books"]);
+        }
+      });
   }
 
 }
